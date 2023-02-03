ab5539ddcb3b6d674fb4330d8a2a10970d91e86c
Raeif <Raeif@DESKTOP-P75IRAE>
2023-02-03 16:38:06 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Jogo_de_adivinhacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int numero_aleatorio = random.Next(1, 101);
+            int Acerto;
+
+            Console.WriteLine("Adivinhe o número entre 1 e 100");
+            do
+            {
+                Acerto = int.Parse(Console.ReadLine());
+                if (Acerto < numero_aleatorio)
+                {
+                    Console.WriteLine("O número é maior. Tente novamente");
+
+                }
+                else if (Acerto > numero_aleatorio)
+                {
+                    Console.WriteLine("O número é menor. Tente novamente");
+                }
+            } while (Acerto != numero_aleatorio);
+            Console.WriteLine("Parabéns, você adivinhou o número.");
+            Console.ReadLine();
+        }
+    }
+}
